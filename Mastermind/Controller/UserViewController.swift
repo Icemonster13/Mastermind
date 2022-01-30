4209d01f9c38b87eafa692c10af309694c14d6a6
Michael Pascucci <mdpascucci@live.com>
2022-01-30 09:48:12 -0500

    Added a User View Controller to access and show the user's progress persistently throughout the game

---
+++ Mastermind/Controller/UserViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserViewController.swift
+//  Mastermind
+//
+//  Created by Michael & Diana Pascucci on 1/30/22.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
